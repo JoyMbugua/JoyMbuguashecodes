6096d4c673a69932d1c2a249c306bcce50f17607
JoyMbugua <joymbugua123@gmail.com>
2024-05-22 01:44:03 +0300

    Added functional js usi ng APIs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+import axios from "axios";
+function showCurrentWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = math.round(temperature);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "0de40f3ac6t7b9b23817fc4oa7443d4f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=imperial`;
+  axios.get(apiUrl).then(showCurrentWeather);
+}
+
 function Submit(event) {
   event.preventDefault();
   let SearchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = SearchInput.value;
 }
 
 let SearchForm = document.querySelector("#search-form");
 SearchForm.addEventListener("submit", Submit);
+searchCity("Sydney");
