9b7a6dfb80f4df99bb21079c99a9d050647cc29e
JoyMbugua <joymbugua123@gmail.com>
2024-05-24 20:44:52 +0300

    added some elements

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ function showCurrentWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = math.round(temperature);
-  let city = document.querySelector("#city");
-  city.innerHTML = response.data.city;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 }
 function searchCity(city) {
-  let apiKey = "0de40f3ac6t7b9b23817fc4oa7443d4f";
+  let apikey = "0de40f3ac6t7b9b23817fc4oa7443d4f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=imperial`;
   axios.get(apiUrl).then(showCurrentWeather);
 }
