98c289aa987a809a922f401e1bffcce571b2dbdc
JoyMbugua <joymbugua123@gmail.com>
2024-05-25 01:56:27 +0300

    imported axios fixed errors and added a module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 function showCurrentWeather(response) {
   let temperatureElement = document.getElementById(`current-temperature`);
   let temperature = response.data.temperature.current;
