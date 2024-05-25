a92108dfe0e6771d62816ed84178d775b40cd373
JoyMbugua <joymbugua123@gmail.com>
2024-05-25 17:11:55 +0300

    trying to fix error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import axios from "./axios";
 function showCurrentWeather(response) {
   let currentTemperature = document.getElementById(`current-temperature`);
   let temperature = response.data.temperature.current;
